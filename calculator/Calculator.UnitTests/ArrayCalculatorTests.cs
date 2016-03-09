26310031bdf4a0746399f3f5a6674d4a657cd5a7
Marco van Deursen <marco.vandeursen@greenchoice.nl>
2016-03-09 14:54:22 +0100

    kleine wijzigingen calculator

--- calculator/Calculator.UnitTests/ArrayCalculatorTests.cs
+++ calculator/Calculator.UnitTests/ArrayCalculatorTests.cs
@@ -1,29 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using Xunit;
-
-namespace Calculator.UnitTests
+﻿namespace Calculator.UnitTests
 {
+    using System;
+    using System.Collections.Generic;
+    using Xunit;
+
     public class ArrayCalculatorTests
     {
+        public static IEnumerable<object[]> InputData
+        {
+            get
+            {
+                return new[]
+                {
+                    // Input array - Expected value
+                    new object[] {new int[] {}, 0},
+                    new object[] {new[] {42}, 42},
+                    new object[] {new[] {1, 2}, 3},
+                    new object[] {new[] {43, 33, 5423, 2}, 5501},
+                    new object[] {new[] {43, 23, 121, 3, 5, 643, -43, 4321, 3}, 5119}
+                };
+            }
+        }
+
         [Fact]
         public void Add_WithNullParameter_ThrowsArgumentNullException()
         {
             // Arrange
-            ArrayCalculator sut = new ArrayCalculator();
-            
+            var sut = new ArrayCalculator();
+
             // Act
-            Action act = () => sut.Add(null);
+            var ex = Record.Exception(() => sut.Add(null));
 
             // Assert
-            Assert.Throws<ArgumentNullException>(() => act());
+            Assert.NotNull(ex);
+            Assert.IsType<ArgumentNullException>(ex);
         }
 
         [Fact]
         public void Add_WithMaxIntPlus1_ThrowsException()
         {
             // Arrange
-            ArrayCalculator sut = new ArrayCalculator();
+            var sut = new ArrayCalculator();
             int[] input = {int.MaxValue, 1};
 
             // Act
@@ -38,29 +55,13 @@ namespace Calculator.UnitTests
         public void Add_SomeValidValues_SumOfValues(int[] input, int expected)
         {
             // Arrange
-            ArrayCalculator sut = new ArrayCalculator();
+            var sut = new ArrayCalculator();
 
             // Act
-            int actual = sut.Add(input);
+            var actual = sut.Add(input);
 
             // Assert
             Assert.Equal(expected, actual);
-        }
-
-        public static IEnumerable<object[]> InputData
-        {
-            get
-            {
-                return new[]
-                {
-                    // Input array - Expected value
-                    new object[] {new int[] {} , 0},
-                    new object[] {new int[] {42} , 42},
-                    new object[] {new int[] {1,2} , 3},
-                    new object[] {new int[] {43,33,5423,2} , 5501},
-                    new object[] {new int[] { 43, 23, 121, 3, 5, 643, 43, 4321, 3}, 5205}
-                };
-            }
         }
     }
 }
--- calculator/Calculator/ArrayCalculator.cs
+++ calculator/Calculator/ArrayCalculator.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Linq;
-
-namespace Calculator
+﻿namespace Calculator
 {
+    using System;
+    using System.Linq;
+
     /// <summary>
-    /// Mahtematical operations on an array of integers
+    ///     Mahtematical operations on an array of integers
     /// </summary>
     public class ArrayCalculator
     {
         /// <summary>
-        /// Adds the specified input.
+        ///     Adds the specified input.
         /// </summary>
         /// <param name="input">The input.</param>
         /// <returns>The sum of all the intergers in the array</returns>
         /// <exception cref="System.ArgumentNullException">When parameter is null.</exception>
-        /// <exception cref="System.OverflowException">When the sum is more then <see cref="int.MaxValue"/>.</exception>
+        /// <exception cref="System.OverflowException">When the sum is more then <see cref="int.MaxValue" />.</exception>
         public int Add(int[] input)
         {
             if (input == null)
@@ -22,7 +22,7 @@ namespace Calculator
                 throw new ArgumentNullException(nameof(input));
             }
 
-            int returnValue = 0;
+            var returnValue = 0;
 
             returnValue = input.Aggregate(returnValue, (current, i) => checked(current + i));
 
